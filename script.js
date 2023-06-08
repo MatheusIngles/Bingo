d15692a8d8c6023fc52806b5e97bbb61f4fd5d9a
Matheus Endlich Silveira <126157948+MatheusIngles@users.noreply.github.com>
2023-06-08 03:26:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,14 +6,15 @@ function Start(){
 
     VideoStarter.play();
 
-    PlayMusic()
 
-    setTimeout(ColorChange, 7900)   
+    setTimeout(PlayMusic, 6000)
+
+    setTimeout(ColorChange, 0)   
 
     function ColorChange(){
         var ColorH1 = document.getElementById("tela_inicial")
         
-        ColorH1.style.color = "black"
+        ColorH1.style.color = "#00000000"
     }
 
     
@@ -48,4 +49,50 @@ function PauseMusic() {
         BingoMusic.pause();
         PausePlay.src = "https://cdn.pixabay.com/photo/2016/02/01/12/33/play-1173551_1280.png";
     }
+}
+
+var verificacao = false;
+
+function Dialogo() {
+    var DialogoDiv = document.getElementById("Perguntas");
+    var DialogoP = document.getElementById("falas");
+
+    if (DialogoDiv.style.display === "none") {
+        setTimeout(function() {
+            DialogoDiv.style.display = "flex";
+
+            if (!verificacao) {
+                DialogoP.innerHTML = "Bem-vindo, marujo! Sente-se e deixe-me contar uma história dos mares que você ainda não conhece. Sou Jack Marés Profundas McGregor, o orgulhoso capitão deste estabelecimento conhecido como O Barril do Tesouro. Aqui, além de um bom gole, você encontrará diversão de sobra.";
+
+                setInterval(function() {
+                    DialogoP.innerHTML = "Vejo que você é novo por estas águas, um rosto fresco navegando pelo desconhecido. Permita-me apresentar-lhe o tesouro escondido nas brincadeiras que temos a bordo. Hoje, lhe convido para embarcar em uma aventura diferente, um jogo que nos faz sentir a emoção de ganhar como se estivéssemos encontrando uma ilha cheia de ouro: o bingo pirata!";
+                }, 10000);
+
+                setInterval(function() {
+                    DialogoP.innerHTML = "Ouça bem, jovem aventureiro, pois neste bingo você descobrirá um universo repleto de números misteriosos e prêmios que farão seu coração bater mais forte. Junte-se a nós nesta noite e teste sua sorte. Será como atirar um dado contra as ondas revoltas do mar. Quem sabe se, com um pouco de astúcia, você não irá se tornar o mais temido vencedor do bingo dos sete mares?";
+                }, 20000);
+
+                setInterval(function() {
+                    DialogoP.innerHTML = "";
+                }, 30000);
+
+                verificacao = true;
+            } else{
+                bottons(DialogoP,DialogoDiv)
+            }
+            
+        }, 0);
+    } else {
+        DialogoDiv.style.display = "none";
+    }
+}
+
+
+function bottons(DialogoP) {
+    var butoes = document.getElementById("butoes")
+
+    DialogoP.remove();
+    butoes.style.height = "60%"
+    butoes.display = "flex"
+
 }
